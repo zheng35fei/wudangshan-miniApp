bb9e56c1288f0ab87a4dd0e9e1a6c36522d32297
sendinfo_zhpf <sendinfo_zhpf@git.cloud.tencent.com>
2018-08-14 14:01:57 +0800

    添加组件

--- pages/park/list.js
+++ pages/park/list.js
@@ -1,4 +1,42 @@
 Page({
-  data: {},
+  data: {
+    dropdownSelectData: {
+      active: false,
+      selectedNav: 0,
+      listData: [
+        {
+          nav: '测试',
+          // 'filter' || 'sort'
+          listType: 'sort' ,
+          selectedItem: '',
+          data: [
+            {
+              thumb:
+                'https://zos.alipayobjects.com/rmsportal/NTuILTPhmSpJdydEVwoO.png',
+              title: '标题文字1',
+            },
+            {
+              thumb:
+                'https://zos.alipayobjects.com/rmsportal/NTuILTPhmSpJdydEVwoO.png',
+              title: '标题图片',
+            }
+          ],
+        },
+        {
+          nav: '测试',
+          selectedItem: '',
+          listType: 'filter' ,
+          data: [
+            {
+              title: '标题文字2',
+            },
+            {
+              title: '标题图片',
+            }
+          ],
+        },
+      ],
+    },
+  },
   onLoad() {},
 });
