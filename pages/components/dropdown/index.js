e177e654dc7973a1e60d041f7b239d22e72257e4
sendinfo_zhpf <sendinfo_zhpf@git.cloud.tencent.com>
2018-08-14 19:40:38 +0800

    列表页筛选分类效果

--- pages/components/dropdown/index.js
+++ pages/components/dropdown/index.js
@@ -1,18 +1,27 @@
 export default {
   _onNavItemTap(e) {
     const index = e.currentTarget.dataset.index;
-
+    console.log(e, index)
     if (this.onDropdownNavItemTap) {
       this.onDropdownNavItemTap(e, index);
     } else {
       console.warn('no onDropdownNavItemTap method');
     }
   },
+  _catchListSortItemTap(e) {
+    const { groupIndex, index, parentIndex, sortItem } = e.currentTarget.dataset;
+
+    if (this.catchDropdownSortItemTap) {
+      this.catchDropdownSortItemTap(e, groupIndex, parentIndex, index, sortItem);
+    } else {
+      console.warn('no catchDropdownSortItemTap method');
+    }
+  },
   _catchListItemTap(e) {
-    const { index, parentIndex, title } = e.currentTarget.dataset;
+    const { index, parentIndex, filter, title } = e.currentTarget.dataset;
 
     if (this.catchDropdownNavItemTap) {
-      this.catchDropdownNavItemTap(e, parentIndex, index, title);
+      this.catchDropdownNavItemTap(e, parentIndex, index, filter, title);
     } else {
       console.warn('no catchDropdownNavItemTap method');
     }
@@ -24,4 +33,12 @@ export default {
       console.warn('no catchDropdownBgTap method');
     }
   },
+  _catchListSortConfirm(e) {
+    const { parentIndex } = e.currentTarget.dataset;
+    if (this.catchDropdownSortConfirm) {
+      this.catchDropdownSortConfirm(e, parentIndex);
+    } else {
+      console.warn('no catchDropdownSortConfirm method');
+    }
+  }
 };
--- pages/park/list.js
+++ pages/park/list.js
@@ -1,5 +1,9 @@
+import filterComonpent from '../components/dropdown';
 Page({
+  ...filterComonpent,
   data: {
+    sortKey:'',
+    labelId:'',
     dropdownSelectData: {
       active: false,
       selectedNav: 0,
@@ -7,17 +11,60 @@ Page({
         {
           nav: '测试',
           // listType是'sort'： data是一个对象
-          listType: 'sort' ,
-          selectedItem: '',
+          listType: 'sort',
           data: [{
               thumb:
                 'https://zos.alipayobjects.com/rmsportal/NTuILTPhmSpJdydEVwoO.png',
               title: '标题文字1',
+              selectedItem: '',
               sortList: [{
-                id:0,
+                labelId:0,
                 name:'全部'
               },{
-                id:1,
+                labelId:1,
+                name:'自然景观自然景观自然景观自然景观自然景观'
+              },{
+                labelId:2,
+                name:'12312321321321321321'
+              },{
+                labelId:3,
+                name:'自然景观'
+              },{
+                labelId:4,
+                name:'自然景观'
+              },{
+                labelId:5,
+                name:'自然景观'
+              },{
+                labelId:6,
+                name:'自然景观'
+              }]
+            },
+            {
+              thumb:
+                'https://zos.alipayobjects.com/rmsportal/NTuILTPhmSpJdydEVwoO.png',
+              title: '标题文字1',
+              selectedItem: '',
+              sortList: [{
+                labelId:7,
+                name:'全部'
+              },{
+                labelId:8,
+                name:'自然景观自然景观自然景观自然景观自然景观'
+              },{
+                labelId:9,
+                name:'12312321321321321321'
+              },{
+                labelId:10,
+                name:'自然景观'
+              },{
+                labelId:11,
+                name:'自然景观'
+              },{
+                labelId:12,
+                name:'自然景观'
+              },{
+                labelId:13,
                 name:'自然景观'
               }]
             }],
@@ -28,15 +75,78 @@ Page({
           listType: 'filter' ,
           data: [
             {
-              title: '标题文字2',
+              val: '标题文字2',
+              key: 1
             },
             {
-              title: '标题图片',
+              val: '标题图片',
+              key: 2
             }
           ],
-        },
+        }
       ],
     },
   },
-  onLoad() {},
+  onLoad() {
+
+  },
+  // 展开下拉
+  onDropdownNavItemTap(e, index) {
+    const { selectedNav, active } = this.data.dropdownSelectData;
+
+    let nextactive = !active;
+    if (selectedNav !== index) {
+      nextactive = true;
+    }
+
+    this.setData({
+      'dropdownSelectData.active': nextactive,
+      'dropdownSelectData.selectedNav': index
+    });
+  },
+
+  // 选择筛选
+  catchDropdownNavItemTap(e, parentIndex, index, filter, title) {
+    const { listData } = this.data.dropdownSelectData;
+    const data = listData[parentIndex];
+
+    data.selectedItem = index;
+    data.nav = title;
+
+    this.setData({
+      sortKey: filter,
+      'dropdownSelectData.active': false,
+      ['dropdownSelectData.listData['+parentIndex+'].selectedItem']: index,
+      ['dropdownSelectData.listData['+parentIndex+'].nav']: title
+    });
+  },
+
+  // 选择分类
+  catchDropdownSortItemTap(e, groupIndex, parentIndex, index, sortItem){
+    const { listData } = this.data.dropdownSelectData;
+    this.setData({
+      ['dropdownSelectData.listData['+ parentIndex +'].data['+groupIndex+'].selectedItem'] : index
+    })
+
+  },
+
+  // 分类确认
+  catchDropdownSortConfirm(e, parentIndex){
+    let labelIdArr = [];
+    const { listData } = this.data.dropdownSelectData;
+    listData[parentIndex].data.forEach(function(t, i){
+      labelIdArr.push(t.sortList[t.selectedItem].labelId)
+    });
+    this.setData({
+      labelId: labelIdArr.join(','),
+      'dropdownSelectData.active': false
+    })
+  },
+
+  // 点击遮罩关闭下拉
+  catchDropdownBgTap(e) {
+    this.setData({
+      'dropdownSelectData.active': false
+    });
+  },
 });
