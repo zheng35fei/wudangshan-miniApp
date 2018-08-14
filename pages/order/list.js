30576e87572cf4ecefa1416ac1354572c2f69b62
yanghaiyu <546488261@qq.com>
2018-08-14 14:09:47 +0800

    提交首页和订单页

--- pages/order/list.js
+++ pages/order/list.js
@@ -1,4 +1,24 @@
 Page({
-  data: {},
-  onLoad() {},
+  data: {
+    order:1,
+    allOrder:[
+      {
+        find:'景区',
+        status:'0',
+        data:'2018-08-14',
+        price:'200',
+        title:'蹦极（武当山快乐谷旅游区）',
+        num:'1'
+      }
+    ]
+
+
+  },
+  clickOrder(event){
+    console.log(event)
+    this.setData({
+      order: event.target.dataset.num
+    })
+  },
+
 });
